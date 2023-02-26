bdc794b493001a33196fa68d8b6b825c015c5bf7
rafed01 <3659@holbertonstudents.com>
2023-02-26 21:41:27 +0100

    0x0A

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,10 @@
+#ifndef __SLIDE_LINE__
+#define __SLIDE_LINE__
+
+#include <stdio.h>
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
+int slide_line(int *line, size_t size, int direction);
+
+#endif
+
