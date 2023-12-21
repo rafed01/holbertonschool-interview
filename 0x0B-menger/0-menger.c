1a5dee5c553f5b6ee27d45ae78da154fb8cfdd40
rafed01 <3659@holbertonstudents.com>
2023-12-21 18:00:12 +0100

    0x0b

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,41 @@
+#include "menger.h"
+
+/**
+ * menger - Draws a 2D Menger Sponge
+ * @level: Depth of Menger Sponge to print
+ * Return: None
+ */
+
+void menger(int level)
+{
+	int X, Y, d, dim = 1;
+
+	if (level >= 0)
+	{
+		for (X = 0; level > X; X++)
+		{
+			dim *= 3;
+		}
+
+		for (X = 0; dim > X; X++)
+		{
+			for (Y = 0; dim > Y; Y++)
+			{
+				for (d = dim / 3; d > 0; d /= 3)
+				{
+					if ((X % (d * 3)) / d == 1 && (Y % (d * 3)) / d == 1)
+						break;
+				}
+				if (d)
+				{
+					printf(" ");
+				}
+				else
+				{
+					printf("#");
+				}
+			}
+			printf("\n");
+		}
+	}
+}
